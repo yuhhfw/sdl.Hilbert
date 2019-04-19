a1e306a1cc9132dd4318fff357041b35c0838e8c
Yuujiro Tai <tai.y.aa@m.titech.ac.jp>
2019-04-19 17:41:40 +0900

    update about rotation

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -2,6 +2,7 @@ package jp.ac.titech.itpro.sdl.hilbert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,16 +16,22 @@ public class MainActivity extends AppCompatActivity {
     private HilbertView hilbertView;
     private Button decButton;
     private Button incButton;
-
+    private final static String KEY_ORDER = "MainActivity.order";
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
         orderView = findViewById(R.id.order_view);
         hilbertView = findViewById(R.id.hilbert_view);
         decButton = findViewById(R.id.dec_button);
         incButton = findViewById(R.id.inc_button);
+
+        if(savedInstanceState != null){
+            order = savedInstanceState.getInt(KEY_ORDER);
+        }
 
         decButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,12 +49,22 @@ public class MainActivity extends AppCompatActivity {
                 display();
             }
         });
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume");
         display();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_ORDER, order);
     }
 
     private void display() {
